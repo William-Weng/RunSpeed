3cc6816d4efe7df739713cb78dad8f9e5d8678ca
William Weng <linuxice0609@gmail.com>
2025-04-12 15:37:36 +0800

    - 加上Obj-C

---
+++ Code/billion.m
@@ -0,0 +1,13 @@
+int main(int argc, const char * argv[]) {
+
+    @autoreleasepool {
+
+        int index = 0;
+        int count = 1000000000;
+
+        while (index < count) {
+            index += 1;
+        }
+    }
+    return 0;
+}
